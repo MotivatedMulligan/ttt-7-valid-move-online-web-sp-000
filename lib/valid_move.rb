390ae578227c86d257a025581d8d1e702c7a65b5
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-18 22:22:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (index == 1..9) && (position_taken? == true)
+  if 
+    index == 1..9
+    true
+    elsif
+    (index == 1..9) && (position_taken? == true)
    true
   else 
    false
