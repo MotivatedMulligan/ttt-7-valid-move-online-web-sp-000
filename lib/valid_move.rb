a6fadc6a4940a295156db7b765e0f572da45d384
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-18 22:27:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,14 +3,13 @@ def valid_move?(board, index)
   if 
     index == 1..9 && (board[index] == "" || board[index] == " ")
    true
- 
   else 
    false
   end
 end
 # returns true for a valid position on an empty board (FAILED - 1)
 # returns true for a valid position on a non-empty board (FAILED - 2)
-# returns nil or false for an occupied position (FAILED - 3)
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
  def position_taken?(board, index)
