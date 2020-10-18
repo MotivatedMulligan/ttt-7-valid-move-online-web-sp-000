b809c8e3916e454b820d3eec6033ebad231cd558
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-18 22:30:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if 
-    index == 1..9 && board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    index == 1..9 && 
    true
   else 
    false
